3dfbe1e5f2a836954bd30b93a88fa8f96e18954e
Leandro Frederico <leandro_frederico@hotmail.com>
2023-07-21 16:54:43 -0300

    google authentication

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const {data: session } = useSession()
 
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProvider = async () => {
+    const setUpProviders = async () => {
       const res = await getProviders()
       setProviders(res)
     }
 
-    setProvider()
+    setUpProviders()
   })
 
   return (
@@ -33,9 +33,10 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -74,7 +75,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+let isConnected = false
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true)
+
+  if(isConnected){
+    return console.log('MongoDB is already connected')
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: process.env.MONGODB_NAME,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    isConnected = true
+    console.log('MongoDB connected!')
+  } catch (error) {
+    console.log(error)
+  }
+}
