7a48c2451a15d07a386f0f29e7cc95447ca1e0a5
Leandro Frederico <leandro_frederico@hotmail.com>
2023-07-23 21:14:08 -0300

    profile page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react'
 import PromptCard from './PromptCard'
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  console.log(data)
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
